71e3b8f07a26d9b86ada4a1825d1630857f2f625
Adiva <prayoga.adiva@gmail.com>
2023-12-28 23:45:17 +0800

    :lipstick: Lucide React

---
+++ src/components/NoteListControl.jsx
@@ -0,0 +1,3 @@
+import Icon from "../components/Icon";
+          <Icon name="List" color="white" size={24} strokeWidth={2} />
+          <Icon name="LayoutGrid" color="white" size={24} strokeWidth={2} />
