71e0067e09add12fdb7d63b7ed4e0ce701ac4215
Adiva <prayoga.adiva@gmail.com>
2023-12-29 23:11:55 +0800

    :recycle: Improvement

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,48 @@
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+
 import Icon from "../components/Icon";
+
+import PropTypes from "prop-types";
+
+function Navbar({ userLogged, handleUserLogout, checkUserLogged }) {
+  useEffect(() => {
+    checkUserLogged();
+  }, []);
+  return (
+    <header>
+      <nav className="container">
+        <div className="logo">
+          <Link to="/">Adiva Notes</Link>
+        </div>
+        <div className="navbar-menu">
+          {userLogged && (
+            <>
+              <div className="archive">
+                <Link to="/archives">
                   <Icon
                     name="Archive"
                     color="white"
                     size={24}
                     strokeWidth={2}
                   />
+                </Link>
+              </div>
+              <div className="logout" onClick={handleUserLogout}>
                 <Icon name="LogOut" color="#FF6767" size={24} strokeWidth={2} />
               </div>
+            </>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+Navbar.propTypes = {
+  userLogged: PropTypes.bool.isRequired,
+  handleUserLogout: PropTypes.func.isRequired,
+  checkUserLogged: PropTypes.func.isRequired,
+};
+
+export default Navbar;
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -3,7 +3,7 @@ function NotFoundPage() {
     <section className="not-found-section">
       <div className="container">
         <h1>404</h1>
-        <p>Halaman tidak ditemukan</p>
+        <p>Page Not Found</p>
       </div>
     </section>
   );
