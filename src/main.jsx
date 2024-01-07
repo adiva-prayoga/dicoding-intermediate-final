af5b6beecb61b92a3c813c2981cb413147f5bbf8
Adiva <prayoga.adiva@gmail.com>
2024-01-08 05:35:51 +0800

    :recycle: Auth Context

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
 import "./styles/style.css";
 import { ThemeProvider } from "./contexts/ThemeContext.jsx";
 import { LocaleProvider } from "./contexts/LocaleContext.jsx";
+import { AuthProvider } from "./contexts/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <AuthProvider>
       <LocaleProvider>
         <ThemeProvider>
           <App />
         </ThemeProvider>
       </LocaleProvider>
-    </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
